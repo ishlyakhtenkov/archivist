31c06d5c285f90b5c23f0324f07f2bafd57c1457
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2023-03-25 14:15:48 +0300

    init commit

---
+++ src/main/java/ru/javaprojects/archivist/ArchivistApplication.java
@@ -0,0 +1,13 @@
+package ru.javaprojects.archivist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/javaprojects/archivist/ArchivistApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javaprojects.archivist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

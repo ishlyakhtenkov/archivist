431b2acd9d78e7aafa042f4b142f9d627dd7efaf
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2023-03-29 18:20:37 +0300

    Refactor users javascript

---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,34 @@
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            let token = $("meta[name='_csrf']").attr("content");
+            let header = $("meta[name='_csrf_header']").attr("content");
+            xhr.setRequestHeader(header, token);
+        }
+    }
+});
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+function successToast(message) {
+    $.toast({
+        heading: 'Success',
+        text: message,
+        showHideTransition: 'slide',
+        position: 'bottom-right',
+        icon: 'success'
+    })
+}
+
+function failToast(message) {
+    $.toast({
+        heading: 'Error',
+        text: message,
+        showHideTransition: 'slide',
+        position: 'bottom-right',
+        icon: 'error'
+    })
+}
---
+++ src/main/resources/static/js/users.js
@@ -0,0 +1,75 @@
+function enableUser(checkbox, id) {
+    let enabled = checkbox.checked;
+    let name = checkbox.dataset.name;
+    $.ajax({
+        url: "users/" + id,
+        type: "PATCH",
+        data: "enabled=" + enabled
+    }).done(function () {
+        successToast('User ' + name + ' was ' + (enabled ? 'enabled' : 'disabled'));
+    }).fail(function () {
+        $(checkbox).prop('checked', !enabled);
+        failToast('Failed to ' + (enabled ? 'enable' : 'disable') + ' user ' + name);
+    });
+}
+
+$('#deleteModal').on('show.bs.modal', function(e) {
+    let name = $(e.relatedTarget).data('bs-name');
+    let id = $(e.relatedTarget).data('bs-id');
+    $(e.currentTarget).find('#deleteModalLabel').text('Delete user ' + name + ' ?');
+    $(e.currentTarget).find('#deleteModalUserId').val(id);
+    $(e.currentTarget).find('#deleteModalUserName').val(name);
+});
+
+function deleteUser() {
+    let deleteModal = $('#deleteModal');
+    let id = deleteModal.find('#deleteModalUserId').val();
+    let name = deleteModal.find('#deleteModalUserName').val();
+    deleteModal.modal('toggle');
+    $.ajax({
+        url: "users/" + id,
+        type: "DELETE"
+    }).done(function () {
+        $('#user-row-' + id).remove();
+        updatePaginationSummary();
+        successToast('User ' + name + ' was deleted');
+    }).fail(function () {
+        failToast('Failed to delete user ' + name);
+    });
+}
+
+function updatePaginationSummary() {
+    let paginationSummary = $('#pagination-summary');
+    let paginationSummaryText = paginationSummary.text();
+    let words = paginationSummaryText.split(' ');
+    let firstEntryNumber = 0;
+    let lastEntryNumber = 0;
+    let totalEntries = 0;
+    let wordsNumberCounter = 0;
+    for (let i = 0; i < words.length; i++) {
+        if (isNumber(words[i])) {
+            wordsNumberCounter++;
+            if (wordsNumberCounter === 1) {
+                firstEntryNumber = words[i];
+            } else {
+                words[i] = words[i] - 1;
+                if (wordsNumberCounter === 2) {
+                    lastEntryNumber = words[i];
+                } else {
+                    totalEntries = words[i];
+                }
+            }
+        }
+    }
+    if (totalEntries < firstEntryNumber) {
+        window.location.href = window.location.pathname;
+    } else if (lastEntryNumber < firstEntryNumber) {
+        window.location.reload();
+    } else {
+        paginationSummary.text(words.join(' '));
+    }
+}
+
+function isNumber(n) {
+    return /^-?[\d.]+(?:e-?\d+)?$/.test(n);
+}

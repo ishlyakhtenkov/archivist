c27ce8c0600987a4572f7a9e8fe32e3e39f9a450
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2023-04-06 15:19:21 +0300

    Refactor users javascript

--- src/main/resources/static/js/users.js
+++ src/main/resources/static/js/users.js
@@ -1,38 +1,40 @@
-window.onload = function() {
-    checkUserSuccessDelete();
-    checkUserActionHappened();
+const userDeleteParam = 'deleteSuccess';
 
+window.onload = function() {
+    checkUserActionHappened();
 };
 
-function checkUserSuccessDelete() {
+function checkUserActionHappened() {
+    let actionSpan = $("#action-span");
+    if (actionSpan.length) {
+        successToast(`User ${actionSpan.data('name')} was ${actionSpan.data('action')}`);
+    } else {
+        checkUserDeleteHappened();
+    }
+}
+
+function checkUserDeleteHappened() {
     let queryString = window.location.search;
     let urlParams = new URLSearchParams(queryString);
-    if (urlParams.has('deleteSuccess')) {
-        successToast('User ' + urlParams.get('deleteSuccess') + ' was deleted');
-        urlParams.delete('deleteSuccess');
+    if (urlParams.has(userDeleteParam)) {
+        successToast(`User ${urlParams.get(userDeleteParam)} was deleted`);
+        urlParams.delete(userDeleteParam);
         let url = (urlParams.toString().length > 0) ? (window.location.pathname + '?' + urlParams) : window.location.pathname;
         window.history.replaceState(null, null, url);
-        setupPageLinks();
+        fixPaginationLinks();
     }
 }
 
-function checkUserActionHappened() {
-    let span = $("#action-span");
-    if (span.length) {
-        successToast('User ' + span.data('name') + ' was ' + span.data('action'));
-    }
-}
-
-function setupPageLinks() {
-    let pageLinks = $("a.page-link");
-    for (let i = 0; i < pageLinks.length; i++) {
-        let pageLink = pageLinks[i];
-        let ref = pageLink.href.toString();
-        let urlParts = ref.split('?');
+function fixPaginationLinks() {
+    let paginationLinks = $("a.page-link");
+    for (let i = 0; i < paginationLinks.length; i++) {
+        let paginationLink = paginationLinks[i];
+        let url = paginationLink.href.toString();
+        let urlParts = url.split('?');
         let urlParams = new URLSearchParams(urlParts[1]);
-        urlParams.delete('deleteSuccess');
-        ref = urlParts[0] + '?' + urlParams;
-        pageLink.href = ref;
+        urlParams.delete(userDeleteParam);
+        url = urlParts[0] + '?' + urlParams;
+        paginationLink.href = url;
     }
 }
 
@@ -43,21 +45,18 @@ function enableUser(checkbox, id) {
         url: "users/" + id,
         type: "PATCH",
         data: "enabled=" + enabled
-    }).done(function () {
-        successToast('User ' + name + ' was ' + (enabled ? 'enabled' : 'disabled'));
-    }).fail(function (data) {
-        if (data.status === 0) {
-            window.location.reload();
-        }
+    }).done(function() {
+        successToast(`User ${name} was ${enabled ? 'enabled' : 'disabled'}`);
+    }).fail(function(data) {
         $(checkbox).prop('checked', !enabled);
-        failToast('Failed to ' + (enabled ? 'enable' : 'disable') + ' user ' + name);
+        handleError(data, `Failed to ${enabled ? 'enable' : 'disable'} user ${name}`);
     });
 }
 
 $('#deleteModal').on('show.bs.modal', function(e) {
-    let name = $(e.relatedTarget).data('bs-name');
-    let id = $(e.relatedTarget).data('bs-id');
-    $(e.currentTarget).find('#deleteModalLabel').text('Delete user: ' + name);
+    let name = $(e.relatedTarget).data('name');
+    let id = $(e.relatedTarget).data('id');
+    $(e.currentTarget).find('#deleteModalLabel').text(`Delete user: ${name}`);
     $(e.currentTarget).find('#deleteModalUserId').val(id);
     $(e.currentTarget).find('#deleteModalUserName').val(name);
 });
@@ -70,19 +69,15 @@ function deleteUser() {
     $.ajax({
         url: "users/" + id,
         type: "DELETE"
-    }).done(function () {
+    }).done(function() {
         updatePageContent(id, name);
-    }).fail(function (data) {
-        if (data.status === 0) {
-            window.location.reload();
-        }
-        failToast('Failed to delete user ' + name);
+    }).fail(function(data) {
+        handleError(data, `Failed to delete user ${name}`);
     });
 }
 
 function updatePageContent(id, name) {
     $('#user-row-' + id).remove();
-
     let paginationSummary = $('#pagination-summary');
     let paginationSummaryText = paginationSummary.text();
     let words = paginationSummaryText.split(' ');
@@ -131,9 +126,9 @@ function isNumber(n) {
 }
 
 $('#changePasswordModal').on('show.bs.modal', function(e) {
-    let name = $(e.relatedTarget).data('bs-name');
-    let id = $(e.relatedTarget).data('bs-id');
-    $(e.currentTarget).find('#changePasswordModalLabel').text(name ? ('Change password for: ' + name) : 'Change password');
+    let name = $(e.relatedTarget).data('name');
+    let id = $(e.relatedTarget).data('id');
+    $(e.currentTarget).find('#changePasswordModalLabel').text(`Change password ${name ? `for: ${name}` : ''}`);
     if (name) {
         $(e.currentTarget).find('#changePasswordModalUserName').val(name);
     }
@@ -159,14 +154,9 @@ function changePassword() {
             data: "password=" + password
         }).done(function () {
             changePasswordModal.modal('toggle');
-            successToast(name ? 'Password for ' + name + ' was changed' : 'Password was changed');
+            successToast(`Password for ${name} was changed`);
         }).fail(function (data) {
-            if (data.status === 0) {
-                window.location.reload();
-            }
-            let detail = data.responseJSON.detail;
-            detail = detail.replace('changePassword.', '');
-            failToast(name ? ('Failed to change password for ' + name + ':<br>' + detail) : 'Failed to change password' + ':<br>' + detail);
+            handleError(data, `Failed to change password for ${name}`);
         });
     }
 }
@@ -183,34 +173,32 @@ function changePasswordProfile() {
         }).done(function () {
             changePasswordModal.modal('toggle');
             successToast('Password was changed');
-        }).fail(function (data) {
-            if (data.status === 0) {
-                window.location.reload();
-            }
-            let detail = data.responseJSON.detail;
-            detail = detail.replace('changePassword.', '');
-            failToast('Failed to change password' + ':<br>' + detail);
+        }).fail(function(data) {
+            handleError(data, 'Failed to change password');
         });
     }
 }
 
+function handleError(data, message) {
+    if (data.status === 0) {
+        window.location.reload();
+    }
+    let detail = data.responseJSON.detail;
+    if (detail.includes('changePassword')) {
+        detail = detail.replace('changePassword.', '');
+    }
+    failToast(`${message}:<br>${detail}`);
+}
+
 $("#repeat-password").on('keyup', function(){
-    let password = $("#new-password").val();
-    let repeatPassword = $("#repeat-password").val();
-    if (repeatPassword.length && password !== repeatPassword) {
-        $("#repeat-password").addClass('form-control is-invalid');
-        $("#checkPasswordMatch").html('<li>password does not match</li>');
-        $('#submit-with-pass-button').prop('disabled', true);
-    }
-    else {
-        $("#repeat-password").removeClass('form-control is-invalid');
-        $("#repeat-password").addClass('form-control');
-        $("#checkPasswordMatch").html('');
-        $('#submit-with-pass-button').prop('disabled', false);
-    }
+    checkPasswordsMatch();
 });
 
 $("#new-password").on('keyup', function(){
+    checkPasswordsMatch();
+});
+
+function checkPasswordsMatch() {
     let password = $("#new-password").val();
     let repeatPassword = $("#repeat-password").val();
     if (repeatPassword.length && password !== repeatPassword) {
@@ -224,4 +212,4 @@ $("#new-password").on('keyup', function(){
         $("#checkPasswordMatch").html('');
         $('#submit-with-pass-button').prop('disabled', false);
     }
-});
+}

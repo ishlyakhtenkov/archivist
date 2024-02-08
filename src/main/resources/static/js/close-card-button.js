83bfd95a42eb740a92d4e7b5bf9832d3f42521fe
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2024-02-08 09:07:07 +0300

    Configure close-card button

---
+++ src/main/resources/static/js/close-card-button.js
@@ -0,0 +1,15 @@
+let cardPrevUrl = document.referrer ? document.referrer :
+    window.location.origin + window.location.pathname.substring(0, window.location.pathname.indexOf('/', 1));
+if (sessionStorage.getItem('cardPrevUrl') == null) {
+    sessionStorage.setItem('cardPrevUrl', cardPrevUrl);
+}
+
+function closeCard() {
+    let cardPrevUrl = sessionStorage.getItem('cardPrevUrl');
+    if (cardPrevUrl) {
+        sessionStorage.removeItem('cardPrevUrl');
+    } else {
+        cardPrevUrl = window.location.origin + window.location.pathname.substring(0, window.location.pathname.indexOf('/', 1));
+    }
+    window.location.href = cardPrevUrl;
+}
